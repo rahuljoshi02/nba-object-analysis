8f684d9828f768061beae3010d35171b769141a1
rahuljoshi02 <rahul.joshi1208@gmail.com>
2025-09-08 13:44:59 -0700

    ball interpolation for missing frames

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import read_video, save_video
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 from drawers import (
-    PlayerTracksDrawer
+    PlayerTracksDrawer,
+    BallTracksDrawer
 )
 
 def main():
@@ -10,18 +11,32 @@ def main():
 
     # Initialize Tracker
     player_tracker = PlayerTracker("models/player_detector.pt")
+    ball_tracker = BallTracker("models/ball_detector_model.pt")
 
     # Run Tracks
     player_tracks = player_tracker.get_object_tracks(video_frames,
                                                      read_from_stub=True,
                                                      stub_path="stubs/player_track_stubs.pkl")
+    
+    ball_tracks = ball_tracker.get_object_tracks(video_frames,
+                                                 read_from_stub=True,
+                                                 stub_path="stubs/ball_tracker_stubs.pkl")
+    
+    # Remove wrong ball Detections
+
+    ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
+    
+    # Interpolate Ball Tracks
+    ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
 
     # Draw Output
     # Initialize Drawers
     player_tracks_drawer = PlayerTracksDrawer()
+    ball_tracks_drawer = BallTracksDrawer()
 
     #Draw Object Tracks
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output_video.avi")
